54e51a0b2aa891e5748bd852bdbfdc337602d1d4
Wilopez <Wilopez95@gmail.com>
2018-03-23 14:52:11 -0600

    Second commit

--- app/src/main/java/com/example/wilop/topmovies/MainActivity.java
+++ app/src/main/java/com/example/wilop/topmovies/MainActivity.java
@@ -1,47 +1,101 @@
 package com.example.wilop.topmovies;
 
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtResult;
+    List<String> source = new ArrayList<String>();
+    TextView tx;
+
+
+    /*public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String results = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                url =  new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+                while (data != -1){
+                    char current  = (char)data;
+                    results += current;
+                    data = inputStreamReader.read();
+                }
+                return results;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-         getHTML();
-
-
-
-
-
-
+        tx = findViewById(R.id.txt);
+        getHTML();
     }
 
+
+
+
     public void getHTML(){
+
         Ion.with(getApplicationContext())
-                .load("http://www.imdb.com/list/ls064079588/")
+                .load("http://dtlinks.club/?v=652")
+                //.load("http://www.imdb.com/list/ls064079588")
                 .asString()
                 .setCallback(new FutureCallback<String>() {
                     @Override
                     public void onCompleted(Exception e, String result) {
+                        tx.setText(result);
                         Log.d("HTML",result);
                     }
                 });
 
+
     }
+
+    public void HTMLParser(){
+
+
+    }
+
+
+
 
 }
